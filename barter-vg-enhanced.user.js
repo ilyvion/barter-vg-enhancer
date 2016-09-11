1baba344c5b326f654fe84ffdd8a328a55721b4b
Alexander Krivács Schrøder <alexschrod@gmail.com>
2016-09-11 18:33:27 +0200

    Fetch price information from Steam

--- barter-vg-enhanced.user.js
+++ barter-vg-enhanced.user.js
@@ -1,15 +1,18 @@
 // ==UserScript==
 // @name         Barter.vg enhancer
 // @namespace    https://alexanderschroeder.net/
-// @version      0.2
+// @version      0.3
 // @description  Summarizes and compares all attributes in an offer for easy comparison of offer value
+// @homepage     https://github.com/alexschrod/barter-vg-enhancer
 // @author       Alexander Krivács Schrøder
 // @downloadURL  https://alexanderschroeder.net/userscripts/barter-vg-enhancer.user.js
+// @supportURL   https://github.com/alexschrod/barter-vg-enhancer/issues
 // @match        https://barter.vg/u/*/o/*/
 // @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js
 // @connect      store.steampowered.com
 // @grant        GM_getValue
 // @grant        GM_setValue
+// @grant        GM_deleteValue
 // @grant        GM_xmlhttpRequest
 // @noframes
 // ==/UserScript==
@@ -105,6 +108,157 @@
         };
     }
 
+    var APPLICATION_JSON = 'application/json';
+    var JSON_PROTECTION_PREFIX = /^\)\]\}',?\n/;
+    var JSON_START = /^\[|^\{(?!\{)/;
+    var JSON_ENDS = {
+        '[': /]$/,
+        '{': /}$/
+    };
+
+    function isJsonLike(str) {
+        var jsonStart = str.match(JSON_START);
+        return jsonStart && JSON_ENDS[jsonStart[0]].test(str);
+    }
+
+    function isString(value) {
+        return typeof value === 'string';
+    }
+
+    function fromJson(json) {
+        return isString(json) ? JSON.parse(json) : json;
+    }
+
+    function getHeaderFunction(headers) {
+        var keyedHeaders = {};
+        $.each(headers, function(_, value) {
+            var splitValue = value.trim().split(':', 2);
+            if (splitValue.length < 2) {
+                return;
+            }
+            keyedHeaders[splitValue[0].trim()] =
+                splitValue[1].trim();
+        });
+        return function(key) {
+            return keyedHeaders[key] || null;
+        };
+    }
+
+    function defaultHttpResponseTransform(data, headers) {
+        if (!isString(data)) {
+            return data;
+        }
+        var tempData = data.replace(JSON_PROTECTION_PREFIX, '').trim();
+        if (!tempData) {
+            return data;
+        }
+        var contentType = headers('Content-Type');
+        if (contentType && contentType.indexOf(APPLICATION_JSON) === 0 || isJsonLike(tempData)) {
+            data = fromJson(tempData);
+        }
+        return data;
+    }
+
+    function makeHttpGetRequest(url, callback) {
+        GM_xmlhttpRequest({
+            method: 'GET',
+            url: url,
+            onload: function(gmResponse) {
+                var headers = getHeaderFunction(gmResponse.responseHeaders.split('\n'));
+                var responseData = gmResponse.response;
+
+                responseData = defaultHttpResponseTransform(responseData, headers);
+                var response = {
+                    data: responseData,
+                    status: gmResponse.status,
+                    headers: headers,
+                    statusText: gmResponse.statusText
+                };
+
+                callback(true, response);
+            },
+            onerror: function(gmResponse) {
+                var headers = getHeaderFunction(gmResponse.responseHeaders.split('\n'));
+                var responseData = gmResponse.response;
+
+                responseData = defaultHttpResponseTransform(
+                    responseData, headers);
+                var response = {
+                    data: responseData,
+                    status: gmResponse.status,
+                    headers: headers,
+                    statusText: gmResponse.statusText
+                };
+
+                callback(false, response);
+            }
+        });
+    }
+
+    function getSteamPricesFor(steamAppIds, callback) {
+        var gamePrices = {};
+        $.each(steamAppIds, function(index, steamAppId) {
+            // Check if the game's price has been retrieved lately
+            var cachedPriceJson = GM_getValue("priceCache." + steamAppId, null);
+            if (cachedPriceJson === null) {
+                return;
+            } else {
+                var cachedPrice = JSON.parse(cachedPriceJson);
+                var cacheAge = new Date() - new Date(cachedPrice.freshTime);
+                if (cacheAge > 3600 * 1000) {
+                    // Expired
+                    GM_deleteValue("priceCache." + steamAppId);
+                } else {
+                    gamePrices[steamAppId] = {
+                        prices: cachedPrice.prices
+                    };
+                }
+            }
+        });
+        $.each(gamePrices, function(steamAppId) {
+            var index = steamAppIds.indexOf(steamAppId);
+            if (index === -1) {
+                return;
+            }
+            steamAppIds.splice(index, 1);
+        });
+        if (steamAppIds.length === 0) {
+            console.debug("all prices cached, no API call necessary!");
+            callback(true, gamePrices);
+            return;
+        }
+
+        var url = "http://store.steampowered.com/api/appdetails/";
+        var urlParameters = {
+            filters: "price_overview",
+            appids: steamAppIds.join(",")
+        };
+        var urlQuery = $.param(urlParameters);
+        if (urlQuery) {
+            url += '?' + urlQuery;
+        }
+
+        var cb = function(success, response) {
+            if (success) {
+                $.each(response.data, function(key, datum) {
+                    gamePrices[key] = {
+                        prices: datum.data.price_overview
+                    };
+
+                    var cachedPrice = {
+                        freshTime: new Date(),
+                        prices: datum.data.price_overview
+                    };
+                    GM_setValue("priceCache." + key, JSON.stringify(cachedPrice));
+                });
+                callback(true, gamePrices);
+            } else {
+                callback(false, null);
+            }
+        };
+        makeHttpGetRequest(url, cb);
+    }
+
     var steamAppIdRegEx = /\/app\/(\d+)\//;
     var reviewScoreRegEx = /(\d+)% positive of (\d+) user reviews/;
     var tradeableRegEx = /(\d+) Barter.vg users have this tradable/;
@@ -113,6 +267,9 @@
 
     var tradeables = $('.tradables');
     var first = true;
+    var allGames = [];
+    var offeredGames = [];
+    var requestedGames = [];
     $.each(tradeables, function(_, tradeable) {
         var whose = $(tradeable).find('legend strong').html();
 
@@ -138,6 +295,9 @@
             }
 
             var gameInfoChildren = gameInfoLine.children();
+            if (gameInfoChildren.length === 0) {
+                return null;
+            }
             var hasStoreTag = gameInfoChildren[0].tagName === 'ABBR' && gameInfoChildren[0].children.length > 0;
             var startIndex = (hasStoreTag ? 2 : 1) - (hasReviewCount ? 0 : 1);
             var tradeableCount = gameInfoChildren[startIndex].title.match(tradeableRegEx)[1];
@@ -151,7 +311,7 @@
                 bundles = bundleData[2].split('; ');
             }
 
-            return {
+            var game = {
                 element: gameListEntry,
                 steamAppId: steamAppId,
                 reviewScore: {
@@ -165,6 +325,15 @@
                     entries: bundles
                 }
             };
+            if (first) {
+                offeredGames.push(game);
+            } else {
+                requestedGames.push(game);
+            }
+            return game;
+        });
+        $.each(games, function(_, game) {
+            allGames.push(game);
         });
 
         var gameStats = games.reduce(function(previousValue, currentValue, currentIndex, array) {
@@ -208,7 +377,7 @@
 
         var tradeSummary =
             '<p>Trade summary:</p>\
-             <table>\
+             <table style="width: 100%;">\
                <tr>\
                  <th>Games in bundles</th><td>{1}</td>\
                </tr>\
@@ -216,16 +385,19 @@
                  <th>Total bundles</th><td>{8}</td>\
                </tr>\
                <tr>\
-                 <th>Average review score</th><td>{4}%</td>\
+                 <th>Average review score <span title="The more reviews it has, the proportionally larger that game\'s impact on the score" style="border-bottom: dotted 1px; cursor: help; font-size: 16px;">(weighted)</span></th><td>{4}% ({5}%)</td>\
                </tr>\
                <tr>\
-                 <th><span title="The more reviews it has, the proportionally larger that game\'s impact on the score" style="border-bottom: dotted 1px; cursor: help; font-size: 16px;">Average weighted review score</th><td>{5}%</td>\
+                 <th>Number of reviews <span title="The binary logarithm of the number of reviews. A difference of +1 means &quot;twice as popular&quot;, and -1 means &quot;half as popular&quot;." style="border-bottom: dotted 1px; cursor: help; font-size: 16px;">(log<sub>2</sub>)</span></th><td>{10} ({11})</td>\
                </tr>\
                <tr>\
-                 <th>Number of reviews (log<sub>2</sub>)</th><td>{10} ({11})</td>\
+                 <th>Trade ratio (H : W)</th><td>rounded: {6},<br>small: {7},<br>actual: {9}</td>\
                </tr>\
                <tr>\
-                 <th>Trade ratio (H : W)</th><td>rounded: {6},<br>small: {7},<br>actual: {9}</span</td>\
+                 <th>Total price on Steam (ignoring any active discounts)</th><td id="{12}_total_value">Loading...</td>\
+               </tr>\
+               <tr>\
+                 <th>Average price per game on Steam (ignoring any active discounts)</th><td id="{12}_average_value">Loading...</td>\
                </tr>\
              </table>'.format(
             gameStats.games,
@@ -239,7 +411,8 @@
             gameStats.totalBundles,
             gameStats.tradeRatioActual,
             gameStats.voteCount,
-            (Math.log(gameStats.voteCount) / Math.log(2)).toFixed(2));
+            (Math.log(gameStats.voteCount) / Math.log(2)).toFixed(2),
+            first ? "offered" : "requested");
 
         if (first) {
             $(tradeable).after(tradeSummary);
@@ -247,5 +420,71 @@
         } else {
             $(tradeable).before(tradeSummary);
         }
+    });
+
+    var steamIds = [];
+    $.each(allGames, function(_, game) {
+        if ($.inArray(game.steamAppId, steamIds) !== -1) {
+            return;
+        }
+        steamIds.push(game.steamAppId);
+    });
+    getSteamPricesFor(steamIds, function(success, gamePrices) {
+        if (!success) {
+            console.error("Error fetching game prices from Steam");
+            return;
+        }
+        $.each(allGames, function(_, game) {
+            if (game.steamAppId in gamePrices) {
+                var gamePrice = gamePrices[game.steamAppId].prices;
+                if (typeof gamePrice === 'undefined') {
+                    gamePrice = {
+                        final: 0,
+                        initial: 0,
+                        discount_percent: 0
+                    };
+                }
+                game.price = gamePrice;
+                var gameElement = game.element;
+                $(gameElement).css("position", "relative");
+                if (gamePrice.final === 0) {
+                    $(gameElement).append('<div style="position: absolute; top: 0; right: 15px;">Steam Store Price: Free</div>');
+                } else {
+                    $(gameElement).append('<div style="position: absolute; top: 0; right: 15px;">Steam Store Price: {0} {1}</div>'
+                                          .format(gamePrice.currency, gamePrice.discount_percent === 0 ?
+                                                  gamePrice.final / 100.0 :
+                                                  "{0} ({1}% off)".format(gamePrice.final / 100.0, gamePrice.discount_percent)));
+                }
+            } else {
+                console.warn("Missing price for:", game.steamAppId);
+            }
+        });
+
+        var currency = null;
+        var offeredTotal = offeredGames.reduce(function(previousValue, currentValue, currentIndex, array) {
+            if (currency === null && typeof currentValue.price.currency !== 'undefined') {
+                currency = currentValue.price.currency;
+            }
+            return previousValue + currentValue.price.initial;
+        }, 0);
+        var offeredDiscountedTotal = offeredGames.reduce(function(previousValue, currentValue, currentIndex, array) {
+            return previousValue + currentValue.price.final;
+        }, 0);
+
+        var requestedTotal = requestedGames.reduce(function(previousValue, currentValue, currentIndex, array) {
+            if (currency === null && typeof currentValue.price.currency !== 'undefined') {
+                currency = currentValue.price.currency;
+            }
+            return previousValue + currentValue.price.initial;
+        }, 0);
+        var requestedDiscountedTotal = requestedGames.reduce(function(previousValue, currentValue, currentIndex, array) {
+            return previousValue + currentValue.price.final;
+        }, 0);
+
+        $("#offered_total_value").html('{0} {1} ({2})'.format(currency, offeredDiscountedTotal / 100.0, offeredTotal / 100.0));
+        $("#requested_total_value").html('{0} {1} ({2})'.format(currency, requestedDiscountedTotal / 100.0, requestedTotal / 100.0));
+
+        $("#offered_average_value").html('{0} {1} ({2})'.format(currency, (offeredDiscountedTotal / offeredGames.length / 100.0).toFixed(2), (offeredTotal / offeredGames.length / 100.0).toFixed(2)));
+        $("#requested_average_value").html('{0} {1} ({2})'.format(currency, (requestedDiscountedTotal / requestedGames.length / 100.0).toFixed(2), (requestedTotal / requestedGames.length / 100.0).toFixed(2)));
     });
 })();
